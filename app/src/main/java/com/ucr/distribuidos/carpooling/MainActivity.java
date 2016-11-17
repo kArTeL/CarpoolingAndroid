12dcfe056ff4174e47f3057f028e352f6d2b32cd
Neil García Vargas <Neil@Neils-MacBook-Pro.local>
2016-11-16 18:51:02 -0600

    added jade dependency

--- app/src/main/java/com/ucr/distribuidos/carpooling/MainActivity.java
+++ app/src/main/java/com/ucr/distribuidos/carpooling/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ucr.distribuidos.carpooling;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ACLMessage subscription = new ACLMessage(ACLMessage.SUBSCRIBE);
+
     }
 }
